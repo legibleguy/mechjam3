f304298e862f179fea548ad091e2e95b6a8f966a
Andy <solovyovandy@yandex.ru>
2022-06-25 08:59:57 -0600

    initial

---
+++ Source/MechJam/MechJam.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MechJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MechJam, "MechJam" );
---
+++ Source/MechJam/MechJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MechJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHJAM_API AMechJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
